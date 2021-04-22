d9c29840197388681ca3f5d2f1714d0749e896ab
Isaac Sokari <isaacsokari@gmail.com>
2021-04-22 13:40:43 +0100

    feat: added quill, and created an editor instance

---
+++ src/components/TextEditor/index.tsx
@@ -0,0 +1,21 @@
+import React, { useCallback } from 'react';
+import Quill from 'quill';
+
+import 'quill/dist/quill.snow.css';
+
+const TextEditor = () => {
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = '';
+
+    const editor = document.createElement('div');
+    wrapper.append(editor);
+
+    new Quill(editor, { theme: 'snow' });
+  }, []);
+
+  return <div id="quillContainer" ref={wrapperRef}></div>;
+};
+
+export default TextEditor;
