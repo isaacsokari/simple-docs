b743d6727c63937a7313119c3bde8a476b73dfb0
Isaac Sokari <isaacsokari@gmail.com>
2021-04-22 19:13:14 +0100

    refactor: added server to the repository

---
+++ server/Document.js
@@ -0,0 +1,5 @@
+const { Schema, model } = require('mongoose');
+
+const Document = new Schema({ _id: String, data: Object });
+
+module.exports = model('Document', Document);
---
+++ server/server.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Document = require('./Document');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('Connected to DB'))
+  .catch((error) => console.error(error));
+
+const io = require('socket.io')(3001, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const defaultValue = '';
+
+io.on('connection', (socket) => {
+  socket.on('get-document', async (documentId) => {
+    const document = await FindOrCreateDocument(documentId);
+
+    socket.join(documentId);
+    socket.emit('load-document', document.data);
+
+    socket.on('send-changes', (delta) => {
+      socket.broadcast.to(documentId).emit('receive-changes', delta);
+    });
+
+    socket.on('save-document', async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function FindOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+
+  return Document.create({ _id: id, data: defaultValue });
+}
