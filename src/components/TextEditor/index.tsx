98e657c51047701261930364f200dc89d6b444fc
Isaac Sokari <isaacsokari@gmail.com>
2021-04-22 14:12:50 +0100

    feat: added toolbar options
    added options using quill modules property

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -3,6 +3,18 @@ import Quill from 'quill';
 
 import 'quill/dist/quill.snow.css';
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: {} }, { background: {} }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+];
+
 const TextEditor = () => {
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
@@ -12,7 +24,7 @@ const TextEditor = () => {
     const editor = document.createElement('div');
     wrapper.append(editor);
 
-    new Quill(editor, { theme: 'snow' });
+    new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
   }, []);
 
   return <div className="container" ref={wrapperRef}></div>;
